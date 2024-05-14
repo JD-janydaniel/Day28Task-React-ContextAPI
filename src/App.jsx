d1addaa094b9091914c1b7392eee3334d0386069
JD-janydaniel <janydaniel59@gamil.com>
2024-05-14 06:53:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,107 @@
+import React, { createContext, useState } from 'react';
+import Cart from './Components/Cart/Cart';
+export const myContext = createContext('')
+const App = () => {
+  let products = [
+    {
+      id: 1,
+      title: "iPhone 9",
+      description: "An apple mobile which is nothing like apple",
+      price: 549,
+      discountPercentage: 12.96,
+      rating: 4.69,
+      stock: 94,
+      brand: "Apple",
+      category: "smartphones",
+      thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+      images: [
+        "/public/img1a.jpg",
+        "/public/img1b.jpg",
+        "/public/img1a.jpg",
+        "/public/img1b.jpg",
+        "/public/img1a.jpg",
+      ],
+    },
+    {
+      id: 2,
+      title: "iPhone X",
+      description:
+        "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+      price: 899,
+      discountPercentage: 17.94,
+      rating: 4.44,
+      stock: 34,
+      brand: "Apple",
+      category: "smartphones",
+      thumbnail: "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
+      images: [
+        "/public/img2a.jpg",
+        "/public/img2b.jpg",
+        "/public/img2c.jpg",
+        "/public/img2d.jpg",
+      ],
+    },
+    {
+      id: 3,
+      title: "Samsung Universe 9",
+      description:
+        "Samsung's new variant which goes beyond Galaxy to the Universe",
+      price: 1249,
+      discountPercentage: 15.46,
+      rating: 4.09,
+      stock: 36,
+      brand: "Samsung",
+      category: "smartphones",
+      thumbnail: "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
+      images: ["/public/img3a.jpg"],
+    },
+    {
+      id: 4,
+      title: "OPPOF19",
+      description: "OPPO F19 is officially announced on April 2021.",
+      price: 280,
+      discountPercentage: 17.91,
+      rating: 4.3,
+      stock: 123,
+      brand: "OPPO",
+      category: "smartphones",
+      thumbnail: "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
+      images: [
+        "/public/img4a.jpg",
+        "/public/img4b.jpg",
+        "/public/img4c.jpg",
+        "/public/img4d.jpg",
+        "/public/img4b.jpg",
+      ],
+    },
+    {
+      id: 5,
+      title: "Huawei P30",
+      description:
+        "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+      price: 499,
+      discountPercentage: 10.58,
+      rating: 4.09,
+      stock: 32,
+      brand: "Huawei",
+      category: "smartphones",
+      thumbnail: "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
+      images: [
+        "/public/img5a.jpg",
+        "/public/img5b.jpg",
+        "/public/img5c.jpg",
+      ],
+    },
+  ];
+  const [data,setData]=useState(products);
+  return (
+    <div>
+      <h1 className='text-center roboto-black-italic'>React useContext Task</h1>
+      <myContext.Provider value={[data,setData]}>
+        <Cart />
+      </myContext.Provider>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Cart/Card.jsx
@@ -0,0 +1,136 @@
+import React, { useContext } from "react";
+import { myContext } from "../../App";
+
+const Card = () => {
+  const [data, setData] = useContext(myContext);
+  const totalPrice = data.reduce(
+    (total, data) => total + data.price * (data.quantity || 1),
+    0
+  );
+  const totalQuantity = data.reduce(
+    (total, data) => total + (data.quantity || 1),
+    0
+  );
+  const handleInc = (id, quantity) => {
+    setData((curr) => {
+      return curr.map((element) => {
+        if (element.id === id) {
+          return { ...element, quantity: element.quantity + 1 || quantity + 1 };
+        }
+        return element;
+      });
+    });
+  };
+  const handleDec = (id, quantity) => {
+    setData((curr) => {
+      return curr.map((element) => {
+        if (element.id === id && quantity > 1) {
+          return { ...element, quantity: element.quantity - 1 || quantity - 1 };
+        }
+        return element;
+      });
+    });
+  };
+  const handleRemove = (id) => {
+    setData(data.filter((element) => element.id !== id));
+  };
+  return (
+    <>
+      <div className="container p-5">
+        <div className="row p-5">
+          <div className="col-12">
+            {data.map((element, index) => {
+              return (
+                <div key={index}>
+                  <div
+                    className="card mb-5 bg-bg-body-tertiary shadow overflow-auto "
+                    style={{ Width: " 400rem" }}
+                  >
+                    <div className="row g-0">
+                      <div className="col-md-3">
+                        <div className="img-box text-center ">
+                        <img
+                          src={element.images[0]}
+                          class="img-fluid rounded mt-5 "
+                          alt="..."
+                        />
+                        </div>
+                      </div>
+                      <div className="col-md-4">
+                        <div className="card-body">
+                          <h3 className="card-title fs-1 ">{element.title}</h3>
+                          <h5 className="card-title fs-2 ">{element.brand}</h5>
+                          <p className="card-text fs-3 lead">
+                            {element.description}
+                          </p>
+                        </div>
+                      </div>
+                      <div className="col-md-5">
+                        <div className="card-body d-flex flex-wrap  justify-content-around align-items-center  mt-5 ">
+                          <button
+                            className="btn btn-outline-success btn-md-lg  fs-1"
+                            onClick={() =>
+                              handleInc(element.id, element.quantity || 1)
+                            }
+                          >
+                            {" "}
+                            +
+                          </button>
+                          <p className="fs-1 border border-info p-1 m-1 rounded-1 ">
+                            {element.quantity || 1}
+                          </p>
+                          <button
+                            className="btn btn-outline-success btn-lg fs-1  "
+                            onClick={() =>
+                              handleDec(element.id, element.quantity || 1)
+                            }
+                          >
+                            -
+                          </button>
+                          <span className="fs-2 p-3 ">${element.price}</span>
+                        </div>
+                        <div className="d-flex justify-content-center align-items-end  m-5 ">
+                          <button
+                            className=" btn btn-danger fs-2 "
+                            onClick={() => handleRemove(element.id)}
+                          >
+                            Remove
+                          </button>
+                        </div>
+                      </div>
+
+                      <div className="row ">
+                        <div className="col-12">
+                          <hr className="ms-2"></hr>
+                          <div className="d-flex justify-content-between align-content-center flex-wrap   ">
+                            <h3 className="text-muted">SHIPPING:</h3>
+                            <h3>FREE</h3>
+                          </div>
+                          <div className="d-flex justify-content-between align-content-center flex-wrap  ">
+                            <h3 className="text-muted">SUBTOTAL:</h3>
+                            <h3>
+                              $
+                              {element.price * element.quantity ||
+                                element.price}
+                            </h3>
+                          </div>
+                          <hr className="ms-2"></hr>
+                          <div className="d-flex justify-content-between align-content-center flex-wrap ">
+                            <h3 className="fs-2">TOTAL:</h3>
+                            <h3>${totalPrice}</h3>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
